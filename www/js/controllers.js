406ff68e8085a88db9e2be5d17a58de560bc21bf
Luccas Quadros <lucc_70@hotmail.com>
2016-10-20 21:43:16 -0200

    Indicador de semana e contagem regressiva funcionando

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,27 @@
+
 angular.module('app.controllers', [])
 
-.controller('reservarCtrl', ['$scope', '$stateParams', // TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('reservarCtrl', ['$scope', '$stateParams',// TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
+$scope.today= new Date();
+$scope.today.setDate($scope.today.getDate($scope.today));
+$scope.weekFirst= new Date();
+$scope.weekLast= new Date();
+$scope.canTime= false;
+$scope.setTimeBol= function(){
+  canTime=true;
+}
+if($scope.today.getDay()<5&&$scope.today.getHours()<=16)
+  $scope.weekFirst.setTime($scope.today.getTime()+(1-$scope.today.getDay())*86400000);
+else
+  $scope.weekFirst.setTime($scope.today.getTime()+(8-$scope.today.getDay())*86400000);
+
+ $scope.weekFirst.setHours(7);
+ $scope.weekFirst.setMinutes(30);
+ $scope.weekFirst.setSeconds(0);
+ $scope.weekLast.setTime($scope.weekFirst.getTime()+(4*86400000));
+ $scope.timeLeft=($scope.weekFirst.getTime()-$scope.today.getTime())/1000;
+
   $scope.dclass = ["button-stable button-outline","button-stable button-outline","button-stable button-outline","button-stable button-outline","button-stable button-outline"];
     $scope.selectDay = function(i){
       if ($scope.dclass[i] === "button-stable button-outline")
