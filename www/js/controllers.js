ac72c86b197b7570d25ee2beaf76cd3d25e6d46f
Luccas Quadros <lucc_70@hotmail.com>
2016-10-24 22:48:12 -0200

    Reserva funcionando com os inputs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,6 +7,7 @@ $scope.today= new Date();
 $scope.today.setDate($scope.today.getDate($scope.today));
 $scope.weekFirst= new Date();
 $scope.weekLast= new Date();
+$scope.weekDays= [{},{},{},{},{}];
 var DAY_IN_MILIS= 86400000;
 
 if($scope.today.getDay()<4||($scope.today.getDay()===4&&$scope.today.getHours()<=16))
@@ -20,19 +21,31 @@ else
  $scope.weekLast.setTime($scope.weekFirst.getTime()+(4*DAY_IN_MILIS));
  $scope.timeLeft=($scope.weekFirst.getTime()-$scope.today.getTime()-(7*DAY_IN_MILIS))/1000;
  //$scope.timeLeft=30;
-
-  $scope.dclass = ["button-stable button-outline","button-stable button-outline",
-                    "button-stable button-outline","button-stable button-outline",
-                    "button-stable button-outline"];
+ for(i=0;i<5;i++){
+   $scope.weekDays[i].date=new Date();
+   $scope.weekDays[i].date.setTime($scope.weekFirst.getTime()+i*DAY_IN_MILIS);
+   $scope.weekDays[i].dClass = "button-stable button-outline";
+   $scope.weekDays[i].dStr = "";
+ }
     $scope.selectDay = function(i){
-      if ($scope.dclass[i] === "button-stable button-outline")
-        $scope.dclass[i] = "button-positive";
-      else
-        $scope.dclass[i] = "button-stable button-outline";
+      if ($scope.weekDays[i].dClass === "button-stable button-outline"){
+                $scope.weekDays[i].dClass = "button-positive";
+                $scope.weekDays[i].dStr=($scope.weekDays[i].date.getDate()<10?'&txt_dias=0':'&txt_dias=')
+                                          +$scope.weekDays[i].date.getDate()+'%2F'+
+                                        ($scope.weekDays[i].date.getMonth()+1)+'%2F'+
+                                        $scope.weekDays[i].date.getFullYear();
+               console.log($scope.weekDays[i].dStr);
+              }
+    else{
+        $scope.weekDays[i].dClass = "button-stable button-outline";
+        $scope.weekDays[i].dStr='';
+    }
     };
 
     $scope.doLogin=function(){
-      console.log("CPF: "+$scope.requiredCpf + "Nº do Cartão: "+$scope.numCartao);
+      var txtCpf= $scope.requiredCpf.substr(0,3)+'.'+$scope.requiredCpf.substr(3,3)+
+                  '.'+$scope.requiredCpf.substr(6,3)+'-'+$scope.requiredCpf.substr(9,2);
+      console.log("CPF: "+txtCpf + "Nº do Cartão: "+$scope.numCartao);
       $http({
             method: 'POST',
             url: 'https://sistemas.fc.unesp.br/ru/reserva.confirmacao.action',
@@ -40,10 +53,12 @@ else
                   'Content-Type': 'application/x-www-form-urlencoded',
                   'Content-Length': '90'
             },
-            data: 'tipoPeriodoReserva=SUBSIDIO'+
-                  '&txt_dias=' + '04%2F11%2F2016' +
-                  '&txt_cartao=' + $scope.numCartao +
-                  '&txt_cpf=' + '456.020.068-85'+''//$scope.requiredCpf
+            data:   'tipoPeriodoReserva=SUBSIDIO'+
+                     $scope.weekDays[0].dStr + $scope.weekDays[1].dStr +
+                     $scope.weekDays[2].dStr + $scope.weekDays[3].dStr +
+                     $scope.weekDays[4].dStr +
+                    '&txt_cartao=' + $scope.numCartao +
+                    '&txt_cpf=' + txtCpf
 
       }).then(function mySucces(response) {
           console.log(response.data);
